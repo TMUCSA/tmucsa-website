9612e2c55ce2706f6510af2e535354b3e56eae77
Dominic Chen <dominic.chen630@gmail.com>
2024-02-03 00:50:36 -0500

    responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,50 +3,66 @@ import navItems from '../../public/data/nav-items.json'
 import Link from "next/link"
 
 export default function Navbar() {
-    console.log(navItems)
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(0);
 
-    const [fetchItems, setData] = useState([]);
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch('/data/nav-items.json');
-                const data = await res.json();
-                setData(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
-        fetchData();
-}, []);
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
 
-const [isScrolled, setIsScrolled] = useState(false);
+        window.addEventListener('resize', handleResize);
 
-useEffect(() => {
-  const handleScroll = () => {
-    const scrollTop = window.scrollY;
-    const isCurrentlyScrolled = scrollTop > 0;
-    setIsScrolled(isCurrentlyScrolled);
-  };
+        // Initial screen width
+        setScreenWidth(window.innerWidth);
 
-  window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
-  return () => window.removeEventListener('scroll', handleScroll);
-}, []);
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            const isCurrentlyScrolled = scrollTop > 150;
+            setIsScrolled(isCurrentlyScrolled);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
-        <nav className={`w-full top-0 left-0 fixed z-50 transition-all duration-150 ease-in ${isScrolled ? 'bg-black bg-opacity-40' : 'bg-default text-white'}`}>
-            <div className='flex flex-row text-white items-center'>
-                <div class="logo flex pl-32 flex-1">
+        <nav className={`w-full top-0 left-0 fixed z-50 transition-all duration-300 ease-in ${isScrolled ? (screenWidth < 768 ? '' : 'bg-black bg-opacity-40') : (screenWidth < 768 ? '' : 'bg-default')} ${isMenuOpen ? 'bg-black bg-opacity-40': ''}`}>
+            <div className='container mx-auto flex flex-row items-center justify-between py-2'>
+                <div className={`logo ${isMenuOpen || screenWidth < 768 ? 'hidden' : ''}`}>
                     <Link href='/'>
-                        <img className='h-28 w-28 hover:scale-110 transition-all duration-100 ease-in-out' src="/icons/logo5.png" alt="CSA LOGO" />
+                        <img className=' h-24 w-24 hover:scale-110 transition-all duration-100 ease-in-out' src="/icons/logo5.png" alt="CSA LOGO" />
                     </Link>
                 </div>
-                <div class="nav-links flex flex-row flex-1 justify-around text-xl">
-                    {fetchItems.map((route, index) => (
-                        <Link href={route.href} key={index} className='p-4 hover:scale-110 transition-all duration-200 ease-in-out underline-on-hover'>
+                <div className={`nav-links md:flex flex-row space-x-4 ${isMenuOpen ? 'block text-s ml-8 text-white' : 'hidden text-xl text-gray-400'}`}>
+                    {navItems.map((route, index) => (
+                        <Link href={route.href} key={index} className='p-4 hover:scale-110 transition-all duration-200 ease-in-out underline-on-hover hover:text-white hover:-translate-y-1'>
                             <p>{route.text}</p>
                         </Link>
                     ))}
+                </div>
+                <div className="md:hidden">
+                    <button className="text-white focus:outline-none px-6 py-2" onClick={toggleMenu}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            {isMenuOpen ? (
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M6 18L18 6M6 6l12 12" />
+                            ) : (
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M4 6h16M4 12h16M4 18h16" />
+                            )}
+                        </svg>
+                    </button>
                 </div>
             </div>
         </nav>
