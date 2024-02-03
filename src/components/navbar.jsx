65f04c140f66127def1b09700fb7c71a9b91a717
Dominic Chen <dominic.chen630@gmail.com>
2024-02-03 00:05:40 -0500

    more animation stuff (scroll opacity)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,8 +19,22 @@ export default function Navbar() {
         fetchData();
 }, []);
 
+const [isScrolled, setIsScrolled] = useState(false);
+
+useEffect(() => {
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    const isCurrentlyScrolled = scrollTop > 0;
+    setIsScrolled(isCurrentlyScrolled);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => window.removeEventListener('scroll', handleScroll);
+}, []);
+
     return (
-        <nav>
+        <nav className={`w-full top-0 left-0 fixed z-50 transition-all duration-150 ease-in ${isScrolled ? 'bg-black bg-opacity-40' : 'bg-default text-white'}`}>
             <div className='flex flex-row text-white items-center'>
                 <div class="logo flex pl-32 flex-1">
                     <Link href='/'>
