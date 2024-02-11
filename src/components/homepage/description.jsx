4cec0940eeeb5b29b1cb3e9eed6aed5115cf0f5f
Andre <dtandre331@gmail.com>
2024-02-11 18:28:07 -0500

    test

--- src/components/homepage/description.jsx
+++ src/components/homepage/description.jsx
@@ -32,8 +32,6 @@ export default function Description() {
     };
 
     useEffect(() => {
-        console.log("inView:", inView);
-        console.log("animationTriggered:", animationTriggered);
         if (inView && !animationTriggered) {
             setAnimationTriggered(true);
         }
--- src/components/homepage/follow.jsx
+++ src/components/homepage/follow.jsx
@@ -14,9 +14,12 @@ export default function Follow() {
 
     const fetchInstagramData = async () => {
         try {
-            const data = await fetch('/api/instagram-feed');
-            setInstagramData(await data.json());
-        } catch (err) { }
+            const data = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,children{media_url,thumbnail_url}&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`);
+            const feed = await data.json();
+            setInstagramData(feed.data.slice(0, 13));
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     return (
