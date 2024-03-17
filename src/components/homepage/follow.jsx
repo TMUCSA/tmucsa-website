1e20be61ade53e7c5ebd29b2fbf832e637279667
Andre <dtandre331@gmail.com>
2024-03-17 15:36:12 -0400

    api/instagram instead of NEXT_PUBLIC

--- src/components/homepage/follow.jsx
+++ src/components/homepage/follow.jsx
@@ -14,9 +14,9 @@ export default function Follow() {
 
     const fetchInstagramData = async () => {
         try {
-            const data = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,children{media_url,thumbnail_url}&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}&limit=13`);
-            const feed = await data.json(); // limit to 13 api calls. holy fk
-            setInstagramData(feed.data);
+            const data = await fetch('/api/instagram');
+            const feed = await data.json();
+            setInstagramData(feed);
         } catch (err) {
             console.error(err);
         }
