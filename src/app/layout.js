2be03f97bd2e316495f0c0c902e7c83ce4f45bcc
andrearcaina <dtandre331@gmail.com>
2024-09-22 21:48:43 -0400

    fix: cross origin error

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
