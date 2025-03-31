0bda039bac201bf766a8dd56444e9e06ecc5bac6
Dominic Chen <DChen@fgfbrands.com>
2025-03-31 13:26:18 -0400

    started events page

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,10 +1,12 @@
 'use client';
+import LatestEvent from '@/components/events/latestEvent';
 import { useState, useEffect } from 'react';
 
 export default function Events() {
     return (
-        <>
-            <p> events </p>
-        </>
+        <main className=' overflow-x-hidden pt-20'>
+            <h1 className='font-josefin text-center font-black text-6xl uppercase text-white'> events </h1>
+            <LatestEvent />
+        </main>
     );
 }
